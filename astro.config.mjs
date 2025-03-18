6ec33ec0363ba578e31375417aae7bf64100bfe1
Milos Savic <67543570+msavic248@users.noreply.github.com>
2025-03-18 21:24:24 +0100

    Added dark mode switcher with astro icon and video card component skeleton

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,19 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  integrations: [vue(), icon()],
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
